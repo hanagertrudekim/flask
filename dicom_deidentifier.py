c411f6bda8a4ade392f5937b1d9369805855ab63
hana <rlagksk8428@gmail.com>
2023-10-18 06:20:45 +0900

    check deid path

--- dicom_deidentifier.py
+++ dicom_deidentifier.py
@@ -123,8 +123,8 @@ def get_dcm_paths_from_dcm_dir(src_dcm_dir: str) -> List[Path]:
 def prepare_deid_dcm_dir(src_dcm_dir, subj) -> str:
         
     dcm_dir_root = dirname(src_dcm_dir)
-    print("src_dcm_dir: ", src_dcm_dir) /Users/hana/Desktop/Beta/KU39009_20220921/DCM_SARP4_80871040_deid_IN0
-    print("dcm_dir_root: ", dcm_dir_root) /Users/hana/Desktop/Beta/KU39009_20220921
+    print("src_dcm_dir: ", src_dcm_dir)
+    print("dcm_dir_root: ", dcm_dir_root) 
 
     dcm_dir_array = dirname(dcm_dir_root).split(os.path.sep)
     new_dcm_dir_root = os.path.sep.join(dcm_dir_array)
@@ -140,8 +140,8 @@ def prepare_deid_dcm_dir(src_dcm_dir, subj) -> str:
 
     deid_dcm_child_dir = ("_").join([subj, basename(src_dcm_dir).split("_")[1]])
     deid_dcm_dir_child_path = os.path.join(deid_dcm_dir_path, deid_dcm_child_dir)
-    print("deid_dcm_dir_child_path: ", deid_dcm_dir_child_path) /Users/hana/Desktop/Beta/KU39009_deid_20220921/cfaaaaf2-dcbe-4978-98a9-3f355d2a9628_SARP4
-    
+    print("deid_dcm_dir_child_path: ", deid_dcm_dir_child_path)
+
     if not os.path.exists(deid_dcm_dir_path):
         os.mkdir(deid_dcm_dir_path)
         os.system(f'chmod 777 {deid_dcm_dir_path}')
